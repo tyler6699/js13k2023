17c56fd2c6f9d8a7b8c4b60d5171cd75cec63cc5
Ryan Tyler <rtyler@glam.ac.uk>
2023-09-07 01:34:27 +0100

    make enemy hp red

--- assets/js/entity.js
+++ assets/js/entity.js
@@ -193,12 +193,11 @@ function entity(w, h, x, y, angle, type, colour, scale, isButton = false, maxHP 
               }
 
               if(this.type==types.SKELLY || this.type==types.GOB || this.type==types.ROCK || this.type==types.TREE){
-                // HP
                 //Draw HP
                 if(this.hp < this.maxHP && this.alpha==1){
                   ctx.globalAlpha = .7;
                   ctx.fillRect(8, -15, 26, 10);
-                  ctx.fillStyle = this.type==types.ROCK? "#ededed" : "#00dcf8";
+                  ctx.fillStyle = this.type==types.ROCK? "#ededed" : "#00DCF8";
                   ctx.fillStyle = this.type==types.TREE? "green" : ctx.fillStyle;
                   ctx.fillRect(10, -13, (22/this.maxHP)*this.hp, 6);
                 }
